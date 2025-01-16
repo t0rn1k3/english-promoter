10e478a4eb1aae73ec2943bba5dd0fc88ee71bdc
t0rn1k3 <tornikeburjanadze2@gmail.com>
2025-01-16 16:22:30 +0400

    create and style main page component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
+import MainPage from "./components/main -page/MainPage";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">{/* <MainPage /> */}</div>;
 }
 
 export default App;
---
+++ src/components/main -page/MainPage.js
@@ -0,0 +1,15 @@
+import "./MainPage.css";
+import "../../global.css";
+
+function MainPage() {
+  return (
+    <div className="main-page">
+      <div className="logo">
+        <img alt="logo" src="" />
+      </div>
+      <button className="start-button">გამოცდის დაწყება</button>
+    </div>
+  );
+}
+
+export default MainPage;
